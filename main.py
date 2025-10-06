e72500e917f3fb30aa91205ba03c2f919bb836ed
PlazmaBot2000 <guardian9641@gmail.com>
2025-10-06 21:34:40 +0300

    Add statements generator and decorators

--- main.py
+++ main.py
@@ -1,21 +1,61 @@
 import random
 
 #Parameters
+    #Comment: questions_count should be < than obj_count
+    #Comment: statements_count should be < than obj_count
+statements_count = 5
 questions_count = 3
 obj_count = 5
-questions = []
 obj = []
 actions = []
+questions = []
+statements = []
+#                                                      ____  _____ ____ ___  ____      _  _____ ___  ____  ____  
+#                                                     |  _ \| ____/ ___/ _ \|  _ \    / \|_   _/ _ \|  _ \/ ___| 
+#=====================================================| | | |  _|| |  | | | | |_) |  / _ \ | || | | | |_) \___ \====================================
+#=====================================================| |_| | |__| |__| |_| |  _ <  / ___ \| || |_| |  _ < ___) |===================================
+#                                                     |____/|_____\____\___/|_| \_\/_/   \_\_| \___/|_| \_\____/ 
 
-#Functions
+
+
+def print_result_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        print("Result:", result)
+        return result
+    return wrapper
+
+
+
+def print_array_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        for i in range(len(result)):
+            print(result[i])
+        
+        return result
+    return wrapper
+
+
+@print_array_decorator
 def gen_obj():
     for i in range(obj_count):
         obj.append(chr(ord('@')+(i+1)) + ' ')
         actions.append("true")
-        print(obj[i] + "is " + actions[i])
+    return obj, actions
 
+
+#                                                        _____ _   _ _   _  ____ _____ ___ ___  _   _ ____  
+#                                                       |  ___| | | | \ | |/ ___|_   _|_ _/ _ \| \ | / ___| 
+#=======================================================| |_  | | | |  \| | |     | |  | | | | |  \| \___ \==========================================
+#=======================================================|  _| | |_| | |\  | |___  | |  | | |_| | |\  |___) |=========================================
+#                                                       |_|    \___/|_| \_|\____| |_| |___\___/|_| \_|____/ 
+
+
+
+@print_array_decorator
 def gen_questions():
-    obj_for_questions = obj
+    obj_for_questions = obj.copy()
     for i in range(questions_count):
 
         index = random.randint(0, (len(obj_for_questions) - 1)) 
@@ -27,20 +67,39 @@ def gen_questions():
         questions.append(obj_for_questions[index] + prefix + actions[index])
         obj_for_questions.pop(index)
 
-        print(questions[i])
+    return questions
 
-def gen_obj_act_pair():
-    index = random.randint(0, obj_count - 1) 
-    print(index)
-    if random.randint(1, 3) == 1:
-        prefix = "is not "
-    else:
-        prefix = "is "
+
+
+@print_array_decorator
+def gen_statements():
+    obj_for_statements = obj.copy()
+    for i in range(statements_count):
+
+        index = random.randint(0, (len(obj_for_statements) - 1)) 
+        if random.randint(1, 2) == 1:
+            prefix = "is not "
+        else:
+            prefix = ""
         
-    return obj[index] + prefix + actions[index]
+        statements.append(obj_for_statements[index] + prefix + actions[index])
+        obj_for_statements.pop(index)
+    return statements
+    
 
-#Main
+
+
+
+
+#                                                                __  __    _    ___ _   _ 
+#                                                               |  \/  |  / \  |_ _| \ | |
+#===============================================================| |\/| | / _ \  | ||  \| |===========================================================
+#===============================================================| |  | |/ ___ \ | || |\  |===========================================================
+#                                                               |_|  |_/_/   \_\___|_| \_|
+
 print("Objects:")
 gen_obj()
+print("Утверждения:")
+gen_statements()
 print("Правда ли что:")
 gen_questions()
