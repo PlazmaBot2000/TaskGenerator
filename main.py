24bc99f47e3411daa1ac3b2f197fc4be84e3b1bd
PlazmaBot2000 <guardian9641@gmail.com>
2025-10-14 21:14:50 +0300

    Refactoring main.py

--- main.py
+++ main.py
@@ -12,57 +12,51 @@ with open("config.yml", 'r') as cfg:
 #=======================================================| |_  | | | |  \| | |     | |  | | | | |  \| \___ \==========================================
 #=======================================================|  _| | |_| | |\  | |___  | |  | | |_| | |\  |___) |=========================================
 #                                                       |_|    \___/|_| \_|\____| |_| |___\___/|_| \_|____/ 
+def generate_from_file():
+ 
+    with open(config['objects_file_name'], 'r') as objects_file:
+        objects_from_file = [item.strip() for item in objects_file.readlines()]
+    with open(config['actions_file_name'], 'r') as actions_file:
+        actions_from_file = [item.strip() for item in actions_file.readlines()]
 
-def print_pairs_array(a):
+    result_array = []
+
+    for _ in range(config['objects_count']):
+        result_array.append(tuple([objects_from_file.pop(random.randint(0, (len(objects_from_file) - 1))), ' ',
+actions_from_file.pop(random.randint(0, (len(actions_from_file) - 1)))]))
+        
+    return result_array
+
+
+
+def gen_pairs(pairs_array, count):
+    result_array = []
+
+    for _ in range(count):
+        index = random.randint(0, (len(pairs_array) - 1))
+        result_array.append((pairs_array[index][0], random.choice([" ", " не "]), pairs_array[index][2] ))
+        pairs_array.pop(index)
+    return result_array
+
+
+
+def print_pairs(a):
     for i in a:
         print("  " + i[0] + i[1] + i[2])
     print()
 
 
-def generate_from_file(objects_file_name, actions_file_name, count):
-
-    result_array = []
-
-    with open(objects_file_name, 'r') as obj_file:
-        lines = obj_file.readlines()
-        obj_for_generation = [item.strip() for item in lines]
-    with open(actions_file_name, 'r') as actions_file:
-        lines = actions_file.readlines()
-        actions_for_generation = [item.strip() for item in lines]
-
-    for _ in range(count):
-        obj_index = random.randint(0, (len(obj_for_generation) - 1)) 
-        actions_index = random.randint(0, (len(actions_for_generation) - 1)) 
-
-        result_array.append([obj_for_generation[obj_index], ' ', actions_for_generation[actions_index]])
-        obj_for_generation.pop(obj_index)
-        actions_for_generation.pop(actions_index)
-    return result_array
-
-
-def gen_pairs(objects_and_actions_array, count):
-    result_array = []
-
-    for _ in range(count):
-        index = random.randint(0, (len(objects_and_actions_array) - 1))
-        if random.randint(1, 3) == 1:
-            objects_and_actions_array[index][1] = " не "
-        else:
-            objects_and_actions_array[index][1] = " "
-
-        result_array.append(objects_and_actions_array[index])
-        objects_and_actions_array.pop(index)
-    return result_array
 
 
 def new_task():
-    objects_and_actions = generate_from_file(config['objects_file_name'], config['actions_file_name'], config['objects_count']) 
+    objects_and_actions = generate_from_file() 
     statements = gen_pairs(objects_and_actions.copy(), config['statements_count'])
     questions = gen_pairs(objects_and_actions.copy(), config['questions_count'])
+
     print("Утверждения:")
-    print_pairs_array(statements)
+    print_pairs(statements)
     print("Правда ли что:")
-    print_pairs_array(questions)
+    print_pairs(questions)
 
 #                                                                __  __    _    ___ _   _ 
 #                                                               |  \/  |  / \  |_ _| \ | |
